a14c9e1f97f27e5947ef6b6e465f447b35e8ec02
pratikkamejaliya007 <pratikkamejaliya6157@gmail.com>
2024-05-09 21:52:08 +0530

    Todo

--- todo/src/Todo.jsx
+++ todo/src/Todo.jsx
@@ -1,69 +1,75 @@
-import React,{useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Todo() {
+    const [inputValue, setInputValue] = useState('');
+    const [data, setData] = useState([]);
+    const [change, setChange] = useState(false);
+    const [selectedTaskIndex, setSelectedTaskIndex] = useState(null);
 
-    const [inputvalue,setInputvalue]=useState('')
-    const [data,setData]=useState([])
-
-    function add(){
-
-        let newdata={
-            Task:inputvalue
+    function add() {
+        if (inputValue.trim() !== '') {
+            let newdata = {
+                Task: inputValue
+            };
+            if (change) {
+                // If change is true, it means we are updating an existing task
+                const updatedData = [...data];
+                updatedData[selectedTaskIndex].Task = inputValue;
+                setData(updatedData);
+            } else {
+                setData([...data, newdata]);
+            }
+            setInputValue('');
+            setChange(false);
+            setSelectedTaskIndex(null); 
         }
+    }
 
-        setData([...data,newdata])
-        console.log(data )
-        setInputvalue('')
+    function up(index) {
+        setChange(true);
+        setSelectedTaskIndex(index); 
+        setInputValue(data[index].Task); 
     }
 
     useEffect(() => {
-        let prevdata = JSON.parse(sessionStorage.getItem('Todo'));
-        if (prevdata) {
-          setData(prevdata);
-        }
-      }, []);
-    
-    useEffect(()=>{
+        sessionStorage.setItem('Todo', JSON.stringify(data));
+    }, [data]);
 
-        
-    let prevdata=JSON.parse(sessionStorage.getItem("Todo"))
-
-    if(prevdata){
-        setData(prevdata)
+    function Delete(index){
+        setData(data.filter((el,i)=> i !== index))
     }
 
-    },[data])
-    
-    localStorage.setItem("Todo",JSON.stringify(data))   
+    return (
+        <>
+            <center>
+                <div style={{ margin: '10px' }}>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        placeholder="Enter Task"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        className="input_1"
+                    />
+                    <button onClick={add}>{change ? <>Update</> : <>Add</>}</button>
+                </div>
+            </center>
 
-
-  return (
-    <>
-
-    <center>
-    <div style={{
-        margin:'10px'
-    }}>
-        <input type="text" value={inputvalue} placeholder='Enter Task' onChange={(e)=>setInputvalue(e.target.value)} className='input_1' />
-
-        <button onClick={add}>add</button>
-    </div>
-    </center>
-    
-    <center>
-        
-        <ul style={{width:'100px'}}>
-            {
-                data && data.map((el)=>(
-                    <li key={el.Task}>{el.Task}</li>
-                ))
-            }
-        </ul>
-        
-    </center>
-
-    </>
-  )
+            <center>
+                <div style={{width:'300px'}}>
+                    {data &&
+                        data.map((el, i) => (
+                            <div key={i} style={{ display: 'flex', alignItems: 'center',justifyContent:'space-between' }}>
+                                <h4 style={{paddingInline:'20px'}}>{el.Task}</h4>
+                                <button onClick={() => up(i)} style={{ background: '#087af4' }}>
+                                    Update
+                                </button>
+                                <button style={{ background: 'red' }} onClick={()=> Delete(i)}>Delete</button>
+                            </div>
+                        ))}
+                </div>
+            </center>
+        </>
+    );
 }
 
-export default Todo
+export default Todo;
